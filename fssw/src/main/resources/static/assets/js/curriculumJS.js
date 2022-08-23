b694727ca43d862545bb8f0a6b82c4b8bb18c91c
backgom1 <dmstjd0214@naver.com>
2022-08-23 20:09:27 +0900

    Update

--- fssw/src/main/resources/static/assets/js/curriculumJS.js
+++
@@ -1,8 +0,0 @@
-$(".curCate").on("click", function(){
-    $(".curCate").css("background", "#fff");
-    $(".curCate").css("color", "#000");
-    $(".curContents").css("display", "none");
-    $(this).css("background", "#2071fa");
-    $(this).css("color", "#fff");
-    $("#" + $(this).attr("role")).css("display", "block");
-});
