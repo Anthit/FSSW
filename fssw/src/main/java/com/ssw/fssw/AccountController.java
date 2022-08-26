aa2f8b1c17373642e0d4f5365fd6db74a9065035
jgy2808 <jgy2808@naver.com>
2022-08-26 21:47:19 +0900

    Add delete files (conflicts)

---
+++ fssw/src/main/java/com/ssw/fssw/AccountController.java
@@ -0,0 +1,43 @@
+package com.ssw.fssw.controller;
+
+import com.ssw.fssw.domain.Account;
+import com.ssw.fssw.service.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AccountController {
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // url에 그냥 치고 들어오는 방식을 get 방식이라고 여길 타고 들어와서 단순히 createMemberForm 파일을 보여줌
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    // form 태그에서 method=post 인것을 submit 버튼 눌렀을 때 이 경로가 호출
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+//        Account account = new Account();
+//        account.setEmail(form.getName());
+//        accountService.join(account);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Account> accounts = accountService.findAccounts();
+        model.addAttribute("members", accounts);
+        return "members/memberList";
+    }
+}
---
+++ fssw/src/main/java/com/ssw/fssw/controller/coummuity/CommunityControllerGY.java
@@ -0,0 +1,35 @@
+package com.ssw.fssw.controller.community;
+
+import com.ssw.fssw.domain.Board;
+import com.ssw.fssw.service.board.BoardService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/board/community")
+public class CommunityController {
+    @Autowired
+    private BoardService boardService;
+    @GetMapping("")
+    public String showList(){
+
+        return "view/community/comList";
+    }
+    @GetMapping("/write")
+    public String showWrite(){
+        return "view/community/comWrite";
+    }
+    @PostMapping("/write")
+    public String write(Board form){
+        log.info("form : " + form.getTitle() + " , " + form.getContent() + " , " + form.getCategory());
+        form.setNick("nick");
+        form.setType('0');
+        boardService.post(form);
+        return "redirect:/community";
+    }
+}
