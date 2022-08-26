28b993a45dedf96335b955cabecc21e5787e0f9f
jgy2808 <jgy2808@naver.com>
2022-08-26 20:44:02 +0900

    Update CurriculumController.java

--- fssw/src/main/java/com/ssw/fssw/controller/curriculum/CurriculumController.java
+++ fssw/src/main/java/com/ssw/fssw/controller/curriculum/CurriculumController.java
@@ -1,29 +1,13 @@
 package com.ssw.fssw.controller.curriculum;
 
-import com.ssw.fssw.service.search.SearchService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-import java.util.Map;
-
-@Slf4j
 @Controller
 @RequestMapping("/curriculum")
 public class CurriculumController {
-    private final SearchService searchService;
-    @Autowired
-    public CurriculumController(SearchService searchService) {
-        this.searchService = searchService;
-    }
-
     @RequestMapping("")
-    public String curriculum(Model model) throws Exception {
-        List<Map.Entry<String, Long>> list = searchService.extractKeyword();
-        model.addAttribute("lists", list);
-        return "view/curriculum/curriculum";
+    public String curriculum(){
+        return "view/curriculum/ curriculum";
     }
 }
