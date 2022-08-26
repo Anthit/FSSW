01ad31a9fa028573779dae627463bc7548fd6d29
jgy2808 <jgy2808@naver.com>
2022-08-26 20:41:30 +0900

    Update CurriculumController.java

--- fssw/src/main/java/com/ssw/fssw/controller/curriculum/CurriculumController.java
+++ fssw/src/main/java/com/ssw/fssw/controller/curriculum/CurriculumController.java
@@ -3,7 +3,6 @@ package com.ssw.fssw.controller.curriculum;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @RequestMapping("/curriculum")
 public class CurriculumController {
     @RequestMapping("")
