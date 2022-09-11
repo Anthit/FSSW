dbfbc1301ad7b985a8abcbc92ea34b5218dee105
backgom1 <dmstjd0214@naver.com>
2022-09-11 17:26:25 +0900

    main페이지 Controller 연동 및 페이지 작성
    해당 페이지 작성하여 연결했고
    추가적으로 팀원 소개 modal 창도 수정

--- fssw/src/main/java/com/ssw/fssw/controller/mypage/MypageController.java
+++ fssw/src/main/java/com/ssw/fssw/controller/mypage/MypageController.java
@@ -1,13 +1,14 @@
 package com.ssw.fssw.controller.mypage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/myPage")
 public class MypageController {
-    @RequestMapping("")
-    public String mypage(){
+    @GetMapping
+    public String myPage(){
         return "view/mypage/mypage";
     }
 
