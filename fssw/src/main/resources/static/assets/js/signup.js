c764560eeb9ecacb96d9acb2f107161660a5f8cb
Praesentia-YKM <ykm8864@naver.com>
2022-09-13 17:12:12 +0900

    수정signup.js
    재수정

---
+++ fssw/src/main/resources/static/assets/js/signup.js
@@ -0,0 +1,49 @@
+function joinform_check(){
+    var email_id=document.getElementById("email_id");
+    var pwd=document.getElementById("password");
+    var repwd=document.getElementById("repwd");
+    var nick = document.getElementById("nick");
+
+    if(email_id == ""){
+        alert("이메일을 입력하세요");
+        email_id.focus(); //커서 깜빡이기
+        return false; //아무것도 반환하지 말아라 아래 코드부터 아무것도 진행하지 말것 이란뜻
+    };
+    if(pwd.value==""){
+        alert("비밀번호를 입력하세요.");
+        pwd.focus();
+        return false;
+    }
+    var pwd_check= /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+    if(!pwd_check.test(pwd.value)){
+        alert("비밀번호는 영문자+숫자+특수문자 조합으로 8~25자리 사용해야 합니다");
+        pwd.focus();
+        return false;
+    };
+
+    function nick_check(){
+        var data={
+            "nick" : $("#nick").val()
+        };
+
+        $.ajax({
+            async: true,
+            type: "POST",
+            data: {
+                email: $("#nick").val()
+            },
+            url :"/signupNick",
+            dataType:'json' ,
+            contentType: "application/json; charset=UTF-8",
+            success: function(count){
+                if(count==1){
+                    $("#nick_check").html("중복된 닉네임이 있습니다");
+                }else{
+                    $("#nick_check").html("");
+                }
+            }
+        });
+    }
+
+}
